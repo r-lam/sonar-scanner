3064564809e5df9b030dcef4cadf9a8920d83120
Ricardo Lam <ricardolam3@gmail.com>
2025-04-21 19:17:05 -0600

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,33 +1,7 @@
 @Library('devsecops_library') _
 
 pipeline {
-  agent {
-    kubernetes {
-      yaml '''
-      apiVersion: v1
-      kind: Pod
-      metadata:
-        labels:
-          jenkins: worker
-      spec:
-        containers:
-        - name: semgrep
-          image: 061051214962.dkr.ecr.us-east-2.amazonaws.com/jenkins/build:v0.0.1
-          resources:
-            requests:
-              cpu: 500m
-              memory: 512Mi
-          limits:
-              cpu: 1000m
-              memory: 2048Mi
-          command: ["/bin/bash", "-c", "cat"]
-          tty: true
-        securityContext:
-          runAsUser: 0
-          fsGroup: 0
-      '''
-    }
-  }
+  agent any
   environment {
     PROJECT = 'https://github.com/veracode/verademo.git'
     PROJECT_ROOT = '.'
@@ -49,15 +23,13 @@ pipeline {
 
     stage('Análisis Semgrep') {
       steps {
-        container('semgrep') {
-          sh '''
-            echo "Ejecutando análisis Semgrep..."
-            semgrep scan ${PROJECT_ROOT} \
-              --config auto \
-              --timeout-threshold 10000 \
-              --json -output semgrep-result.json
-          '''
-        }
+        sh '''
+          echo "Ejecutando análisis Semgrep..."
+          semgrep scan ${PROJECT_ROOT} \
+            --config auto \
+            --timeout-threshold 10000 \
+            --json -output semgrep-result.json
+        '''
       }
     }
   }
